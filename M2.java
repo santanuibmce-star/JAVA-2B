a73793e22a03fa52e425c24a944169958fdcfad8
santanuibmce-star <santanu.ibmce@gmail.com>
2026-06-04 13:42:10 +0530

    some updated code

--- M2.java
+++ M2.java
@@ -1,5 +1,6 @@
 public class M2 {
     public static void main(String[] args) {
-        System.out.println("hiiiiiiiii");
+        System.out.println("helloo");
+        System.out.println("sheela");
     }
 }
